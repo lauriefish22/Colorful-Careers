36b14d3d017a147cb6edb48c8375bea21f58d0c8
Ox Noj <geneveva@gmail.com>
2023-04-11 13:48:54 -0700

    edited html/script

--- landingassets/mainscript.js
+++ landingassets/mainscript.js
@@ -1,7 +1,7 @@
 
 
 // Get dropdown-item value
-let colorValue = document.querySelectorAll(".dropdown-item");
+var colorValue = document.querySelectorAll(".dropdown-item");
 
 // Store value in variable
 let color;
@@ -11,6 +11,7 @@ for (let i = 0; i < colorValue.length; i++) {
     colorValue[i].addEventListener("click", function() {
         color = colorValue[i].innerHTML;
         console.log(color);
+        document.getElementById("color").innerHTML = "You have chosen " + color + "!";
     });
 }
 
@@ -21,5 +22,15 @@ let submitBtn = document.getElementById("colorinput");
 submitBtn.addEventListener("click", function() {
     // Store value
     localStorage.setItem("color", color);
-    console.log("color stored!");
+    // console.log("color stored!");
 });
+
+document.getElementsByClassName("button is-info")[0].addEventListener("click", myFunction);
+
+function myFunction(){
+    var citystate = document.getElementById("citystate").value;
+    localStorage.setItem("citystate", citystate);
+    console.log(citystate);
+}
+
+
