4eea619cd3ece96e0ba428cf070289a7790cbbf5
Ox Noj <geneveva@gmail.com>
2023-04-11 14:40:03 -0700

    edited even listener for color choice dropdown

--- landingassets/mainscript.js
+++ landingassets/mainscript.js
@@ -1,29 +1,29 @@
 
 
-// Get dropdown-item value
-var colorValue = document.querySelectorAll(".dropdown-item");
+// // Get dropdown-item value
+// var colorValue = document.querySelectorAll(".dropdown-item");
 
-// Store value in variable
-let color;
+// // Store value in variable
+// let color;
 
-// Loop through dropdown-items
-for (let i = 0; i < colorValue.length; i++) {
-    colorValue[i].addEventListener("click", function() {
-        color = colorValue[i].innerHTML;
-        console.log(color);
-        document.getElementById("color").innerHTML = "You have chosen " + color + "!";
-    });
-}
+// // Loop through dropdown-items
+// for (let i = 0; i < colorValue.length; i++) {
+//     colorValue[i].addEventListener("click", function() {
+//         color = colorValue[i].innerHTML;
+//         console.log(color);
+//         document.getElementById("color").innerHTML = "You have chosen " + color + "!";
+//     });
+// }
 
-// Get submit button
-let submitBtn = document.getElementById("colorinput");
+// // Get submit button
+// let submitBtn = document.getElementById("colorinput");
 
-// Add click listener to submit button
-submitBtn.addEventListener("click", function() {
-    // Store value
-    localStorage.setItem("color", color);
-    // console.log("color stored!");
-});
+// // Add click listener to submit button
+// submitBtn.addEventListener("click", function() {
+//     // Store value
+//     localStorage.setItem("color", color);
+//     // console.log("color stored!");
+// });
 
 document.getElementsByClassName("button is-info")[0].addEventListener("click", myFunction);
 
@@ -32,5 +32,22 @@ function myFunction(){
     localStorage.setItem("citystate", citystate);
     console.log(citystate);
 }
+
+
+
+var colorInput = document.getElementById("colorinput");
+var colorChoice = document.getElementById("colorchoice");
+var dropdownMenu = document.getElementById("dropdown-menu");
+
+colorInput.addEventListener("click", function () { 
+    var chosenColor = colorChoice.innerHTML;
+    console.log(chosenColor);
+    localStorage.setItem("chosenColor", chosenColor);
+});
+
+dropdownMenu.addEventListener("click", function (event) {
+    var chosenColor = event.target.innerHTML;
+    colorChoice.innerHTML = chosenColor;
+});
 
 
